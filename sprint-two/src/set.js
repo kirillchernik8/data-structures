e51be7f21e880a1be3c340405a5722881ee27e50
Kirill Chernik <kirillchernik8@gmail.com>
2019-02-21 11:07:44 -0500

    Implement Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  return this._storage.hasOwnProperty(item)
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  delete this._storage[item]
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+   1) Add item is constant time as we only assigning the value of the item to the 
+     current storage
+   2) Contains is also constant as object[key] lookups are constant
+   3) So is remove as we only delete the property of the object
+   
  */
