4e6311266f2b07ffeaa96f0ead5d4e5e94af4d86
Kirill Chernik <kirillchernik8@gmail.com>
2019-02-23 10:36:23 -0500

    Set can handle  input objects of any type

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,29 +1,71 @@
 var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = {}; // fix me
+  set._storage = {};
+  set.top = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function (item) {
-  this._storage[item] = item;
-};
+  if (!this.contains(item)) {
+    this._storage[this.top] = item;
+    this.top++;
+  }
+}
 
 setPrototype.contains = function (item) {
-  return this._storage.hasOwnProperty(item)
+  let isItThere = false
+  for (let i = 0; i < Object.values(this._storage).length; i++) {
+    if (Object.values(this._storage)[i] === item) {
+      isItThere = true;
+    }
+  }
+  return isItThere;
 };
 
 setPrototype.remove = function (item) {
-  delete this._storage[item]
+  for (let key in this._storage) {
+    if (this._storage[key] === item) {
+      delete this._storage[key]
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
 
-   1) Add item is constant time as we only assigning the value of the item to the 
-     current storage
-   2) Contains is also constant as object[key] lookups are constant
-   3) So is remove as we only delete the property of the object
+   1) Add item is linear as it relies on the contains method. O(n)     current storage
+   2) Contains is linear as it involves traversal over the values of the Object. O(n)
+   3) Remove is linera as we need to traverse thru the storage to get the element; 
    
  */
+
+let set1 = Set()
+set1.add('asd')
+set1.add(2)
+let blop = {
+  a: 2,
+  b: 5
+}
+let blop1 = {
+  a: 2,
+  b: 7
+}
+set1.add(blop)
+set1.add(blop1)
+set1.add(3)
+set1.add(3)
+set1.add(3)
+set1.remove(3)
+set1.add('Susan Sarandon')
+set1.add('Danny Glover');
+
+
+console.log(set1.contains('Danny Glover'))
+let cat = function (x) {
+  return x
+}
+set1.add(cat)
+set1.add(true)
+console.log(set1)
